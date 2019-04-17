d8b189c6f336698c419392d15722880d4d184620
Hick <Fabian.Hick@EXXETA.com>
2019-04-17 16:35:25 +0200

    Another CORS try

--- serve.py
+++ serve.py
@@ -1,7 +1,9 @@
 from CTFd import create_app, socketio
 
 app = create_app()
-CORS(app, resources=r'api/v1/challenges/attempt')
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+CORS(app, resources={r'api/v1/challenges/attempt': {"origins": "*"}})
 # app.run(debug=True, threaded=True, host="127.0.0.1", port=4000)
 
 socketio.run(app, debug=True, host="127.0.0.1", port=4000)
